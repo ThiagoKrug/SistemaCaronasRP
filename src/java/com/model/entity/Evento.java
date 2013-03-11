e84e0cc712fad9c7ae50117fa04f50c30743c14d
ThiagoKrug <thiagockrug@gmail.com>
2013-03-11 19:19:24 -0300

    adicionada entidade evento

---
+++ src/java/com/model/entity/Evento.java
@@ -0,0 +1,37 @@
+package com.model.entity;
+
+/**
+ *
+ * @author thiago
+ */
+public class Evento {
+    
+    private String titulo;
+    private String inicio;
+    private String fim;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFim() {
+        return fim;
+    }
+
+    public void setFim(String fim) {
+        this.fim = fim;
+    }
+    
+}
